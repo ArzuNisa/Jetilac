3674c2899bf1b5348b82fd579fa888e914c10dc8
İrem Nur Taşci <99333911+iremnurtasci@users.noreply.github.com>
2023-07-16 15:47:29 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/FlutterMultiDexApplication.kt
@@ -0,0 +1,7 @@
+package io.flutter.app
+
+import androidx.multidex.MultiDexApplication
+
+class FlutterMultiDexApplication : MultiDexApplication() {
+    // Sınıfın içine başka kodlar da ekleyebilirsiniz.
+}
